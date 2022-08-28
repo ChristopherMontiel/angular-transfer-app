a0143c2733047c9b22faafa64ddfe959b1b862fb
Christopher Montiel <christopher.montiel.m@gmail.com>
2022-08-28 10:01:59 -0400

    [ADD] Se crea interfaz para Recipient

---
+++ src/app/models/newRecipient.interface.ts
@@ -0,0 +1,10 @@
+export interface RecipientI{
+  name: string;
+  rutNumber: number;
+  rutDv: string;
+  mail: string;
+  phone: string;
+  bank: string;
+  accType: string;
+  accNumber: number;
+}
