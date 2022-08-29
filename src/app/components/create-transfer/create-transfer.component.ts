11c9429a5901be14eab7789e141873db0703f5bc
Christopher Montiel <christopher.montiel.m@gmail.com>
2022-08-28 22:58:08 -0400

    [ADD] Se añade funcionalidad para realizar una nueva transferencia con la API

--- src/app/components/create-transfer/create-transfer.component.ts
+++ src/app/components/create-transfer/create-transfer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TransferI } from 'src/app/models/newTransfer.interface';
 import { NewRecipientService } from 'src/app/services/new-recipient.service';
+import { NewTransferService } from 'src/app/services/new-transfer.service';
 
 @Component({
   selector: 'app-create-transfer',
@@ -9,6 +11,8 @@ import { NewRecipientService } from 'src/app/services/new-recipient.service';
 export class CreateTransferComponent implements OnInit {
 
   nameSelected! : string;
+  rutNumberSelected!: number;
+  rutDvSelected!: string;
   mailSelected! : string;
   bankSelected! : string;
   accTypeSelected! : string;
@@ -16,12 +20,13 @@ export class CreateTransferComponent implements OnInit {
   public recipientList: any;
 
   constructor(
-    private apiRecipients: NewRecipientService
+    private apiRecipients: NewRecipientService,
+    private apiTransfers: NewTransferService
   ) { }
 
   ngOnInit(): void {
     this.apiRecipients.getRecipients().subscribe( (data: any) =>{
-      console.table(data);
+      //console.table(data);
       this.recipientList = data;
     });
   }
@@ -29,24 +34,55 @@ export class CreateTransferComponent implements OnInit {
   selectRecipient(recipientSelected : any): void {
     //Obtener el valor
     try{
-      console.log(recipientSelected.srcElement.value);
+      //console.log(recipientSelected.srcElement.value);
       let recipientFound = this.recipientList.find( (recipient : any)  => recipient.name === recipientSelected.srcElement.value );
-      console.log(recipientFound);
+      //console.log(recipientFound);
       //Asignar variables.
       this.nameSelected = recipientFound.name;
       this.mailSelected = recipientFound.mail;
       this.bankSelected = recipientFound.bank;
       this.accTypeSelected = recipientFound.accType;
+      this.rutNumberSelected = recipientFound.rutNumber ;
+      this.rutDvSelected = recipientFound.rutDv;
     }catch(e){
       this.nameSelected = '';
       this.mailSelected = '';
       this.bankSelected = '';
       this.accTypeSelected = '';
+      this.rutNumberSelected = 0;
+      this.rutDvSelected = '';
       //console.error(e);
     }
+  }
 
+  makeTransfer(amountInput : string) : void {
+    console.log(amountInput);
+    let dat = new Date();
+    let dateNow : string = dat.getFullYear() + '-' + dat.getMonth().toString().padStart(2,'0') + '-' + dat.getDate().toString().padStart(2,'0');
+    let timeNow : string = dat.getHours().toString().padStart(2,'0') + ':' + dat.getMinutes().toString().padStart(2);
+    //console.log("La fecha es: " + dateNow);
+    //console.log("La hora es: " + timeNow);
 
+    let newTransferObject : TransferI = {
+      recName: this.nameSelected,
+      recRutNumber: this.rutNumberSelected,
+      recDv: this.rutDvSelected,
+      recBank: this.bankSelected,
+      recAccType: this.accTypeSelected,
+      amount: parseInt(amountInput),
+      date: dateNow,
+      time: timeNow,
+      originAccount: 0
+    }
+    console.table(newTransferObject);
 
+    this.onCreate(newTransferObject);
+  }
+
+  onCreate(form: TransferI){
+    this.apiTransfers.createNewTransfer(form).subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
